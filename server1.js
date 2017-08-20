cc181e693a24fc59ed42837576f3e997029ea43f
likskodam9380 <30389628+likskodam9380@users.noreply.github.com>
2017-08-20 13:43:15 +0530

    [imad-console] Updates server1.js

--- server1.js
+++ server1.js
@@ -2,3 +2,67 @@ var express = require('express');
 var morgon = require('morgon');
 var path = require('path');
 console.log('hi!...');
+function createTemplate (data)
+{
+var title = data.title; 
+var heading = data.heading;
+var content = data.content;
+var htmlTemplate = `
+   <!doctype html>
+<html>
+    <head>
+<title>${title} </title>
+<meta name="viewport" content="width-device, initial-scale-1" />
+<link href="/ui/style.css" rel="stylesheet" />
+</head>
+<body>
+    <div class="container">
+    <div>
+    <a href="/">Home</a>
+    <hr> 
+    </div>
+    
+<h1>${heading}</h1>
+<div>
+${content}
+</div>
+</div>
+</body>
+</html>
+`;
+return htmlTemplate;
+}
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+});
+
+app.get('/:articleName', function (req, res) {
+    var articleName = req.params.articleName;
+  res.send(createTemplate(articles[articleName]));
+});
+
+app.get('/article-two', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+});
+
+app.get('/article-three', function(req, res) {
+   res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+});
+
+app.get('/ui/style.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+});
+
+app.get('/ui/madi.png', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+});
+
+
+// Do not change port, otherwise your app won't run on IMAD servers
+// Use 8080 only for local development if you already have apache running on 80
+
+var port = 80;
+app.listen(port, function () {
+  console.log(`IMAD course app listening on port ${port}!`);
+});
