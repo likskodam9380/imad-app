4efccbc9acf19f8aa9e54a4b0821e1e16be34911
likskodam9380 <30389628+likskodam9380@users.noreply.github.com>
2017-09-01 22:52:15 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,15 @@
 console.log('Loaded!');
+//chage the text of the maintext div
+var element=document.getElementById('maintext');
+element.innerHTML='This is my webpage';
+
+var img = document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft +10;
+    img.style.marginLeft=marginLeft + 'px';
+    }
+   img.Onclick=function()
+{
+    var interval = setInterval (moveRight, 100);
+};
