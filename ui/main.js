aac5ecef200161e59a3601dcec01859948631ca5
likskodam9380 <30389628+likskodam9380@users.noreply.github.com>
2017-09-03 16:55:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,8 +3,8 @@ var button = document.getElementById('counter');
 
 button.onclick = function ()
 {
-    var request = new XMLHttpRequest ();
-    request.onreadystatechange = function(){
+    var request = new XMLHttpRequest();
+    request.onreadystatechange = function (){
         
             if (request.readystate === XMLHttpRequest.DONE) {
                 
