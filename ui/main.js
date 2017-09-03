8a3a30f1acabf07c4a8d072084e6799314858d9e
likskodam9380 <30389628+likskodam9380@users.noreply.github.com>
2017-09-03 13:46:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,9 @@
 //console.log('Loaded!');
 var button = document.getElementById('counter');
 var counter = 0;
-button.Onclick = function(){
+
+button.Onclick = function()
+{
     counter = counter + 1;
     var span = document.getElementById('count');
     span.innerHTML=counter.toString();
