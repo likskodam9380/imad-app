85081764b4f71a9ea30ba493aebbf9014abf0c16
likskodam9380 <30389628+likskodam9380@users.noreply.github.com>
2017-09-04 23:20:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -108,11 +108,11 @@ app.get('/ui/main.js', function (req, res) {
 });
 
 app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png');
+  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
 var names=[];
-app.get('submit-name/:name', function (req, res) {
+app.get('/submit-name/:name', function (req, res) {
   var name = req.paras.name;
   names.path(name);
   res.send(JSON.stringify(name));
